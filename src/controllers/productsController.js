04104229512312cb18e7e3e6055b3acff986bd03
ElmazMed <elmazgour.mohamed@gmail.com>
2025-05-05 23:50:14 +0100

    added cloudinary upload images

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,4 +1,13 @@
 import Product from "../models/products.schema.js";
+import { v2 as cloudinary } from "cloudinary";
+import { config } from "dotenv";
+
+config();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const addProduct = async (req, res) => {
   const {
@@ -17,17 +26,23 @@ const addProduct = async (req, res) => {
     if (!title) {
       return res.status(400).json({ message: "Please add a title" });
     }
+    const productImgUpload = await cloudinary.uploader.upload(
+      req.files.productImg[0].path
+    );
+    const variantImgUpload = await cloudinary.uploader.upload(
+      req.files.variantImg[0].path
+    );
     const newProduct = new Product({
       title,
       description,
       price,
-      productImg,
+      productImg: productImgUpload.secure_url,
       quantity,
       variantName,
       variant,
       variantPrice,
       variantQuantity,
-      variantImg,
+      variantImg: variantImgUpload.secure_url,
     });
     await newProduct.save();
     return res.status(201).json({ newProduct });
@@ -77,10 +92,27 @@ const updateProduct = async (req, res) => {
     if (!product) {
       return res.status(404).json({ message: "Product not found!" });
     }
-
-    const update = await Product.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
+    let updatedImgs = { ...req.body };
+    if (updatedImgs?.productImg) {
+      const productImgUpload = await cloudinary.uploader.upload(
+        req.files.productImg[0].path
+      );
+      updatedImgs.productImg = productImgUpload.secure_url;
+    }
+    if (updatedImgs?.variantImg) {
+      const variantImgUpload = await cloudinary.uploader.upload(
+        req.files.variantImg[0].path
+      );
+      updatedImgs.variantImg = variantImgUpload.secure_url;
+    }
+    const update = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      updatedImgs,
+      {
+        new: true,
+      }
+    );
     return res.status(200).json({ update });
   } catch (error) {
     console.log(error);
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -7,14 +7,31 @@ import {
   updateProduct,
 } from "../controllers/productsController.js";
 import { protectRoutes } from "../middleware/protect.js";
+import upload from "../middleware/multer.js";
 
 const router = express.Router();
 
 router
-  .post("/add-product", protectRoutes, addProduct)
+  .post(
+    "/add-product",
+    protectRoutes,
+    upload.fields([
+      { name: "productImg", maxCount: 1 },
+      { name: "variantImg", maxCount: 1 },
+    ]),
+    addProduct
+  )
   .get("/", getProducts)
   .get("/:id", getOneProduct)
-  .put("/:id", protectRoutes, updateProduct)
+  .put(
+    "/:id",
+    protectRoutes,
+    upload.fields([
+      { name: "productImg", maxCount: 1 },
+      { name: "variantImg", maxCount: 1 },
+    ]),
+    updateProduct
+  )
   .delete("/:id", protectRoutes, removeProduct);
 
 export default router;
